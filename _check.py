04c4f1962a8c633bb4009ea49f2c551fa9f5e458
jaquimbayoc7 <jaquimbayoc7@gmail.com>
2026-04-29 10:43:23 -0500

    Initial commit: sistema de marketing multiagentes

---
+++ _check.py
@@ -0,0 +1,3 @@
+﻿with open(r'C:\Users\DELL\Documents\GitHub\material-software-automatizacion\04-week\02-session\_writer.py', 'w', encoding='utf-8') as f:
+    f.write('done')
+print('script written')
